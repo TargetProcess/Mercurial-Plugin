95b1da5878347f1b8e215ef016bf4532d64e45fa
Sergey Truhtanov <truhtanov@targetprocess.com>
2012-03-29 13:22:15 +0300

    fixed

--- Tp.Mercurial/Properties/AssemblyInfo.cs
+++ Tp.Mercurial/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Tp.Integration.Plugin.Common;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: PluginAssembly("Mercurial", "Exports revisions from Mercurial and binds source code to TargetProcess user stories, bugs and tasks.", "Source Control")]
--- Tp.Mercurial/Uninstall.bat
+++ Tp.Mercurial/Uninstall.bat
@@ -2,8 +2,8 @@ pushd
 
 cd /d %0\..
 
-NServiceBus.Host.exe /uninstall /serviceName:TpGitIntegrationPlugin
-Tp.Integration.Plugin.UninstallUtil.exe Tp.PubSubStorage Tp.GitIntegration
+NServiceBus.Host.exe /uninstall /serviceName:TpMercurialIntegrationPlugin
+Tp.Integration.Plugin.UninstallUtil.exe Tp.PubSubStorage Tp.Mercurial
 
 popd
 exit 0
